49c16e299fd88977c721063f3bf2522eabbebf93
TosuSW <denisappleid12@gmail.com>
2024-05-12 10:19:53 +0300

    Navbar demo design finished

--- zerogames/app/Global-Components/Navbar/navbar.tsx
+++ zerogames/app/Global-Components/Navbar/navbar.tsx
@@ -1,10 +1,114 @@
 import React from 'react'
 import styles from './page.module.css'
 
-export default function Navbar() {
-  return (
-   <div className={styles.navbar}>
+//components
+import BoxIcon from '@/app/Small-Components/BoxIcon/BoxIcon'
 
-   </div>
-  )
+//icons
+import { FaHome } from "react-icons/fa";
+import { FaGamepad } from "react-icons/fa";
+import { FaUpload } from "react-icons/fa6";
+import { FaListUl } from "react-icons/fa6";
+import { FaDiscord } from "react-icons/fa6";
+
+export default function Navbar() {
+
+    const logo = '/images/logo/logo.png'
+
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.logo}>
+                <img src={logo}></img>
+            </div>
+
+            <div className={styles.hrefs}>
+                <BoxIcon
+                    width='auto'
+                    height='35px'
+                    backgroundColor='transperent'
+                    borderRadius='0.3em'
+                    paddingLeft='1.2em'
+                    paddingRight='1.2em'
+                    titleFontWeight='500'
+                    textTransform='uppercase'
+                    title='Home'
+                    transition='all 0.3s'
+                    color='white'
+                    nav={true}
+                >
+                    <FaHome />
+                </BoxIcon>
+
+                <BoxIcon
+                    width='auto'
+                    height='35px'
+                    backgroundColor='transperent'
+                    borderRadius='0.3em'
+                    paddingLeft='1.2em'
+                    paddingRight='1.2em'
+                    titleFontWeight='500'
+                    textTransform='uppercase'
+                    title='Categories'
+                    transition='all 0.3s'
+                    color='white'
+                    nav={true}
+                >
+                    <FaListUl />
+                </BoxIcon>
+
+                <BoxIcon
+                    width='auto'
+                    height='35px'
+                    backgroundColor='transperent'
+                    borderRadius='0.3em'
+                    paddingLeft='1.2em'
+                    paddingRight='1.2em'
+                    titleFontWeight='500'
+                    textTransform='uppercase'
+                    title='Multiplayer'
+                    transition='all 0.3s'
+                    color='white'
+                    nav={true}
+                >
+                    <FaGamepad />
+                </BoxIcon>
+
+                <BoxIcon
+                    width='auto'
+                    height='35px'
+                    backgroundColor='transperent'
+                    borderRadius='0.3em'
+                    paddingLeft='1.2em'
+                    paddingRight='1.2em'
+                    titleFontWeight='500'
+                    textTransform='uppercase'
+                    title='Recent'
+                    transition='all 0.3s'
+                    color='white'
+                    nav={true}
+                >
+                    <FaUpload />
+                </BoxIcon>
+            </div>
+
+            <div className={styles.navbtn}>
+                <BoxIcon 
+                backgroundColor='red'
+                borderRadius='0.3em'
+                height='40px'
+                width='auto'
+                paddingLeft='1.2em'
+                paddingRight='1.2em'
+                titleFontWeight='600'
+                textTransform='uppercase'
+                iconFontSize='1.1rem'
+                transition='all 0.3s'
+                color='white'
+                title='Join Discord'
+                >
+                    <FaDiscord />
+                </BoxIcon>
+            </div>
+        </div>
+    )
 }
