1985f66489b182a06b922533b877c13b9bcdb7cb
TosuSW <denisappleid12@gmail.com>
2024-05-12 19:45:12 +0300

    Using the banner in the home page

--- zerogames/app/Main-Page-Components/Main/Main.tsx
+++ zerogames/app/Main-Page-Components/Main/Main.tsx
@@ -3,12 +3,23 @@ import styles from './page.module.css'
 
 //components
 import Caruso from './Caruso/Caruso'
+import Banner from '@/app/Small-Components/Banner/Banner'
 
 export default function MainShow() {
     return (
         <div className={styles.main}>
             <div className={styles.caruso}>
                 <Caruso />
+            </div>
+
+            <div className={styles.banner}>
+                <Banner 
+                width='900px'
+                height='200px'
+                borderRadius='0.3em'
+                imgSrc='https://wallpapercave.com/uwp/uwp3536520.jpeg'
+                filter='blur(4px) brightness(0.4)'
+                 />
             </div>
         </div>
     )
