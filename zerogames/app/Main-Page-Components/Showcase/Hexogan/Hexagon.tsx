c9853f38673cdeb3bcf5129fef0a575c57e2112b
TosuSW <denisappleid12@gmail.com>
2024-05-12 12:51:08 +0300

    Created Hexagon component

---
+++ zerogames/app/Main-Page-Components/Showcase/Hexogan/Hexagon.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+
+export default function Hexagon() {
+    return (
+        <div className={styles.hexagon}>
+
+        </div>
+    )
+}
