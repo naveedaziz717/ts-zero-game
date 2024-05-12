568fa71b2225d0280bac1eaaa48be205f9e0a3b6
TosuSW <denisappleid12@gmail.com>
2024-05-12 13:34:47 +0300

    Showcase demo finished

--- zerogames/app/Main-Page-Components/Showcase/Showcase.tsx
+++ zerogames/app/Main-Page-Components/Showcase/Showcase.tsx
@@ -3,6 +3,7 @@ import styles from './page.module.css'
 
 //components
 import ShowcaseNav from './ShowcaseNav/ShowcaseNav'
+import RandomImg from './Hexogan/RandomImg'
 
 export default function Showcase() {
     return (
@@ -13,6 +14,85 @@ export default function Showcase() {
 
             <div className={styles.main}>
                 <ShowcaseNav />
+                <div className={styles.random}>
+                    <div className={styles.first}>
+                        <div className={styles.first1}>
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/uwp/uwp4255255.jpeg'
+                            />
+
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/wp/wp1810408.jpg'
+                            />
+                        </div>
+
+                        <div className={styles.first1}>
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/wp/wp13119966.jpg'
+                            />
+
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/wp/wp7340931.jpg'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={styles.middle}>
+                        <RandomImg
+                            borderRadius='1em'
+                            width='480px'
+                            height='350px'
+                            imgSrc='https://wallpapercave.com/uwp/uwp4228497.jpeg'
+                        />
+                    </div>
+
+                    <div className={styles.last}>
+                        <div className={styles.last1}>
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/wp/wp5912983.png'
+                            />
+
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/wp/wp13384112.jpg'
+                            />
+                        </div>
+                        <div className={styles.last1}>
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/wp/wp13152896.jpg'
+                            />
+
+                            <RandomImg
+                                borderRadius='1em'
+                                width='180px'
+                                height='150px'
+                                imgSrc='https://wallpapercave.com/wp/wp3628771.jpg'
+                            />
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
         </div>
     )
