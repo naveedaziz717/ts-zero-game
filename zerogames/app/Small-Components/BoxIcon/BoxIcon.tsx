fec0dccdf7d2815b127c33f1db0b68700b65151e
TosuSW <denisappleid12@gmail.com>
2024-05-12 10:19:05 +0300

    Update BoxIcon.tsx

--- zerogames/app/Small-Components/BoxIcon/BoxIcon.tsx
+++ zerogames/app/Small-Components/BoxIcon/BoxIcon.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { MouseEventHandler, ReactNode } from 'react'
 import styles from './page.module.css'
 
 interface BoxIconProps {
@@ -16,17 +16,22 @@ interface BoxIconProps {
     paddingRight?: string;
     textTransform?: any;
     transition?: string;
+    iconFontSize?: string;
     nav?: boolean;
+    onClick?: MouseEventHandler<HTMLDivElement>;
+    onHover?:  MouseEventHandler<HTMLDivElement>;
 }
 
 export default function BoxIcon({ width, height, borderRadius, backgroundColor,
     title, children, border, titleFontWeight, 
-    paddingLeft, paddingRight, textTransform, nav, transition, color, fontSize }: BoxIconProps) {
+    paddingLeft, paddingRight, textTransform, nav, transition, color, fontSize, iconFontSize,
+    onClick, onHover }: BoxIconProps) {
     return (
-        <div style={{ width, height, borderRadius, backgroundColor, 
+        <div onClick={onClick} onMouseOver={onHover} style={{ width, height, borderRadius, backgroundColor, 
             border, paddingLeft, paddingRight, transition, color, fontSize }} 
-        className={`${nav ? `${styles.box} ${styles.navhover}` : styles.box}`}>
-            <span className={styles.icon}>{children}</span>
+        className=
+        {`${nav ? `${styles.box} ${styles.navhover}` : `${styles.box} ${styles.default}` }`}>
+            <span style={{fontSize: iconFontSize}} className={styles.icon}>{children}</span>
             <p style={{
                 fontWeight: titleFontWeight,
                 textTransform
@@ -35,3 +40,6 @@ export default function BoxIcon({ width, height, borderRadius, backgroundColor,
         </div>
     )
 }
+
+
+
