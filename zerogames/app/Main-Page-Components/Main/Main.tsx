15cdd9183c97d5b69fb7eb766b3ec220f548b3bd
TosuSW <denisappleid12@gmail.com>
2024-05-12 20:51:32 +0300

    Update Main.tsx

--- zerogames/app/Main-Page-Components/Main/Main.tsx
+++ zerogames/app/Main-Page-Components/Main/Main.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 //components
 import Caruso from './Caruso/Caruso'
 import Banner from '@/app/Small-Components/Banner/Banner'
+import SpecialCaruso from './SpecialCaruso/SpecialCaruso'
 
 export default function MainShow() {
     return (
@@ -13,13 +14,17 @@ export default function MainShow() {
             </div>
 
             <div className={styles.banner}>
-                <Banner 
-                width='900px'
-                height='200px'
-                borderRadius='0.3em'
-                imgSrc='https://wallpapercave.com/uwp/uwp3536520.jpeg'
-                filter='blur(4px) brightness(0.4)'
-                 />
+                <Banner
+                    width='900px'
+                    height='200px'
+                    borderRadius='0.3em'
+                    imgSrc='https://wallpapercave.com/uwp/uwp3536520.jpeg'
+                    filter='blur(4px) brightness(0.4)'
+                />
+            </div>
+
+            <div className={styles.caruso}>
+                <SpecialCaruso />
             </div>
         </div>
     )
