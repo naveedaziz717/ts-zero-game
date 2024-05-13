b997f90ccedec872b87d7e310856c2f6c66b9600
TosuSW <denisappleid12@gmail.com>
2024-05-13 12:47:49 +0300

    Default Caruso functionality demo finished

--- zerogames/app/Main-Page-Components/Main/Caruso/Caruso.tsx
+++ zerogames/app/Main-Page-Components/Main/Caruso/Caruso.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './page.module.css'
 
 //components
@@ -12,19 +12,92 @@ import ProgressBar from '../CarusoComponents/ProgressBar/ProgressBar';
 import { FaDownload } from "react-icons/fa";
 import { IoShieldCheckmarkSharp } from "react-icons/io5";
 
+interface GameProps {
+    imgSrc: string;
+    title: string;
+    description: string;
+    link?: string;
+}
+
+
+
 export default function Caruso() {
+
+    const [page, setPage] = useState<number>(0)
+    const [currentGame, setCurrentGame] = useState<GameProps>()
+    const [games, setGames] = useState<Array<GameProps>>()
+
+    const intervalRef = useRef<NodeJS.Timeout>();
+
+    useEffect(() => {
+        setGames([
+            { title: 'Forza Horizon 6', description: 'Forza Horizon 6 is cool', imgSrc: 'https://wallpapercave.com/wp/wp11907420.jpg' },
+            { title: 'Lego Pirates', description: 'Lego Pirates is cool', imgSrc: 'https://wallpapercave.com/wp/wp9533384.jpg' },
+            { title: 'Lego Star Wars', description: 'Lego Star Wars is cool', imgSrc: 'https://wallpapercave.com/wp/wp6049945.jpg' },
+            { title: 'Sonst of the Forest', description: 'Sonst of the Forest is cool', imgSrc: 'https://wallpapercave.com/wp/wp12152112.jpg' },
+            { title: 'Forza Horizon 5', description: 'Forza is cool', imgSrc: 'https://wallpapercave.com/wp/wp11907420.jpg' },
+            { title: 'Forza Horizon 6', description: 'Forza is cool', imgSrc: 'https://wallpapercave.com/wp/wp11907420.jpg' },
+            { title: 'Forza Horizon 6', description: 'Forza is cool', imgSrc: 'https://wallpapercave.com/wp/wp11907420.jpg' },
+            { title: 'Forza Horizon 6', description: 'Forza is cool', imgSrc: 'https://wallpapercave.com/wp/wp11907420.jpg' },
+            { title: 'Forza Horizon 6', description: 'Forza is cool', imgSrc: 'https://wallpapercave.com/wp/wp11907420.jpg' },
+            { title: 'Forza Horizon 6', description: 'Forza is cool', imgSrc: 'https://wallpapercave.com/wp/wp11907420.jpg' },
+        ])
+    }, [])
+
+    const switchPage = (page: number) => {
+        setPage(page)
+        if(games) {
+            setCurrentGame(games[page]);
+        }
+    }
+
+    useEffect(() => {
+        if (games && page >= 0 && page < games.length) {
+            setCurrentGame(games[page]);
+        }
+        console.log(currentGame)
+    }, [page, games]);
+
+    useEffect(() => {
+        intervalRef.current = setInterval(() => {
+            if(games) {
+                setPage((prevPage) => (prevPage + 1) % games.length);
+            }
+         
+        }, 5000); 
+
+        return () => {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+        };
+    }, [games]);
+
+    const handleProgressBarClick = (pageNumber: number) => {
+        switchPage(pageNumber);
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = setInterval(() => {
+                if(games) {
+                    setPage((prevPage) => (prevPage + 1) % games.length);
+                }
+            }, 5000);
+        }
+    };
+
+
     return (
         <div className={styles.caruso}>
-              <Arrow left={true} />
+            <Arrow left={true} />
 
             <div className={styles.middle}>
                 <div className={styles.title}><p>FEATURED & RECOMMENDED</p></div>
                 <div className={styles.image}>
-                    <img src='https://wallpapercave.com/wp/wp11497733.png'></img>
+                    <img src={currentGame?.imgSrc}></img>
                     <div className={styles.info}>
                         <div className={styles.theinfo}>
-                            <h2 className={styles.infotitle}>Fifa 22</h2>
-                            <p className={styles.infodesc}>FIFA 22 is a football simulation video game published by Electronic Arts. It is the 29th installment in the FIFA series, and was released worldwide on 1 October 2021 for Nintendo Switch, PlayStation 4</p>
+                            <h2 className={styles.infotitle}>{currentGame?.title}</h2>
+                            <p className={styles.infodesc}>{currentGame?.description}</p>
                             <BoxIcon
                                 backgroundColor='green'
                                 width='120px'
@@ -42,7 +115,7 @@ export default function Caruso() {
                         </div>
                     </div>
                 </div>
-               <ProgressBar count={10} />
+                <ProgressBar page={page} setPage={handleProgressBarClick} count={10} />
             </div>
 
             <Arrow left={false} />
--- zerogames/app/Main-Page-Components/Main/CarusoComponents/ProgressBar/ProgressBar.tsx
+++ zerogames/app/Main-Page-Components/Main/CarusoComponents/ProgressBar/ProgressBar.tsx
@@ -1,20 +1,22 @@
 'use client'
 
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import styles from './page.module.css'
 
 interface ProgressBarProps {
-    count: number; 
+    count: number;
+    page: number;
+    setPage: (page: number) => void;
 }
 
-export default function ProgressBar({ count }: ProgressBarProps) {
+export default function ProgressBar({ count, setPage, page }: ProgressBarProps) {
 
     const boxes = Array.from({ length: count }, (_, index) => index);
 
     return (
         <div className={styles.progress}>
             {boxes.map((_, index) => (
-                <div key={index} className={styles.box}></div>
+                <div style={{backgroundColor: index === page ? 'white' : ''}} onClick={() => {setPage(index)}} key={index} className={styles.box}></div>
             ))}
         </div>
     );
