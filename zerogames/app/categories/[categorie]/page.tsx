61ce10f288bc3d816e739cb51082538d9525bb6b
TosuSW <denisappleid12@gmail.com>
2024-05-14 11:06:28 +0300

    Update page.tsx

--- zerogames/app/categories/[categorie]/page.tsx
+++ zerogames/app/categories/[categorie]/page.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
-export default function page() {
+//components
+import CategorieGames from './GetCategorieGames/CategorieGames';
+
+interface params {
+  params: {
+    categorie: string;
+  }
+}
+
+export default function page({ params }: params) {
   return (
     <div className={styles.main}>
+      <p className={styles.categorie}>
+        <Link href={'/'}><span className={styles.nav}>{'Home'}</span></Link> {' > '}
+        <Link href={'/categories'}><span className={styles.nav}>{'Categories'}</span></Link> {' > '}
+        <Link href={'/categories/' + params.categorie}><span className={styles.nav}>{params.categorie}</span></Link>
+      </p>
 
+      <div className={styles.games}>
+        <CategorieGames />
+      </div>
     </div>
   )
 }
