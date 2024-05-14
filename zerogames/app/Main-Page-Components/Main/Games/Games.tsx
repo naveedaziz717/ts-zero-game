addb39743768f3cfae41801671353b4eb24de0cd
TosuSW <denisappleid12@gmail.com>
2024-05-14 11:06:05 +0300

    Update Games.tsx

--- zerogames/app/Main-Page-Components/Main/Games/Games.tsx
+++ zerogames/app/Main-Page-Components/Main/Games/Games.tsx
@@ -3,6 +3,7 @@ import styles from './style.module.css'
 
 //components
 import GameBox from './GameBox/GameBox'
+import GamePages from './Pages/GamePages'
 
 export default function Games() {
     return (
@@ -26,39 +27,7 @@ export default function Games() {
                 <GameBox imgSrc='https://wallpapercave.com/wp/wp11907420.jpg' title='Forza Horizon 6' />
             </div>
 
-            <div className={styles.pages}>
-                <div className={styles.page}>
-                    <p>1</p>
-                </div>
-
-                <div className={styles.page}>
-                    <p>2</p>
-                </div>
-
-                <div className={styles.page}>
-                    <p>3</p>
-                </div>
-
-                <div className={styles.page}>
-                    <p>4</p>
-                </div>
-
-                <div className={styles.page}>
-                    <p>5</p>
-                </div>
-
-                <div className={styles.page}>
-                    <p>6</p>
-                </div>
-
-                <div className={styles.page}>
-                    <p>7</p>
-                </div>
-
-                <div className={styles.page}>
-                    <p>8</p>
-                </div>
-            </div>
+            <GamePages count={10}/>
         </>
     )
 }
