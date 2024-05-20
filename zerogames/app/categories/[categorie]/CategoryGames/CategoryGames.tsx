9fd1c80ac129c6614025df9c54c092c7e2a78e52
TosuSW <denisappleid12@gmail.com>
2024-05-20 13:33:44 +0300

    Default Category Page demo finished

--- zerogames/app/categories/[categorie]/CategoryGames/CategoryGames.tsx
+++ zerogames/app/categories/[categorie]/CategoryGames/CategoryGames.tsx
@@ -6,23 +6,48 @@ import styles from './page.module.css'
 //providers
 import { useCategory } from '@/app/States/Category/CategoryState'
 
+//components
+import GameBox from '@/app/Small-Components/GameBox/GameBox'
+import GamePages from '@/app/Small-Components/Pages/GamePages'
+
 
 interface CategoryProps {
-  params: {categorie: string}
+  params: { categorie: string }
 }
 
 //components
 
-export default function CategoryGames({params} : CategoryProps) {
+export default function CategoryGames({ params }: CategoryProps) {
 
-  const {} = useCategory()
+  const { getCategoryGames, categoryGames, page, setPage } = useCategory()
 
   useEffect(() => {
+    getCategoryGames(1, params.categorie)
+  }, [])
 
-  },[])
+  const noDesc = "The developers unfortunately didn't provide any description for this game, leaving potential players without information about its features, gameplay, or storyline."
 
 
   return (
-    <div>CategoryGames</div>
+    <>
+      <div className={styles.games}>
+        {categoryGames?.map((game, index) => (
+          <GameBox
+            description={game.About.Description ? game.About.Description : game.Extra.Description ? game.Extra.Description : noDesc}
+            key={index}
+            imgSrc={game.General.imgSrc}
+            title={game.General.Title}
+            isDiscount={game.General.gameDiscount}
+            discountOriginalPrice={game.General.DiscountOriginalPrice}
+            finalPrice={game.General.FinalPrice}
+            gamePrice={game.General.GamePrice}
+            keyword1={game.General.Keywords[0].keyword}
+            keyword2={game.General.Keywords[1].keyword}
+          />
+        ))}
+      </div>
+
+      <GamePages defaultPage='/' pushPage={'/categories/' + params.categorie + '/'} onPageChange={setPage} page={page} count={30} />
+    </>
   )
 }
