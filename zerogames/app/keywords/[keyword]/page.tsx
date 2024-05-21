2662efb51e8e1df9742aebac988c8e5a139e8445
TosuSW <denisappleid12@gmail.com>
2024-05-21 16:44:55 +0300

    Created keywords games route

---
+++ zerogames/app/keywords/[keyword]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './page.module.css'
+
+
+import GamePage from '@/app/Global-Components/GamePage/GamePage'
+
+interface CategoryProps {
+    params: { keyword: string }
+}
+
+
+export default function page() {
+  return (
+    <div>page</div>
+  )
+}
