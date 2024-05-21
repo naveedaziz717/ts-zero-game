89f7fced17ea65af561923f8fd44cbfc4f9c7f1d
TosuSW <denisappleid12@gmail.com>
2024-05-21 12:28:14 +0300

    Adding name to the input
    - Fixing autocomplete bug

--- zerogames/app/Small-Components/InputIcon/InputIcon.tsx
+++ zerogames/app/Small-Components/InputIcon/InputIcon.tsx
@@ -17,20 +17,20 @@ interface InputIconProps {
     type: string;
     classN?: string;
     onInput?: FormEventHandler<HTMLInputElement>
-    onKeyDown?:  FormEventHandler<HTMLInputElement>
+    onKeyDown?: FormEventHandler<HTMLInputElement>
     onBlur?: FormEventHandler<HTMLInputElement>;
     onFocus?: FormEventHandler<HTMLInputElement>;
     onChange?: FormEventHandler<HTMLInputElement>;
-    
+
     value?: string
     marginBottom?: string;
 }
 
 const InputIcon = forwardRef<HTMLInputElement, InputIconProps>(({
     backColor, width, height, borderRadius, children, color,
-    fontSize, iconBackColor, iconBorderRadius, 
+    fontSize, iconBackColor, iconBorderRadius,
     title, titleColor, titleFontSize, type, classN, onInput,
-     value, marginBottom, onKeyDown, onBlur, onFocus, onChange
+    value, marginBottom, onKeyDown, onBlur, onFocus, onChange
 }: InputIconProps, ref: Ref<HTMLInputElement>) => {
     return (
         <div className={styles.inputicon}
@@ -50,6 +50,7 @@ const InputIcon = forwardRef<HTMLInputElement, InputIconProps>(({
                 {children}
             </span>
             <input spellCheck='false' autoComplete='off' className={`${styles.theinput} ${classN}`}
+                name='InputIcon'
                 ref={ref}
                 type={type}
                 onInput={onInput}
