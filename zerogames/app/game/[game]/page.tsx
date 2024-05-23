4ee0028bb5e64ebc33f244637bd6ec9c6a940b86
TosuSW <denisappleid12@gmail.com>
2024-05-23 10:23:58 +0300

    Created the game component & ssr fetching

--- zerogames/app/game/[game]/page.tsx
+++ zerogames/app/game/[game]/page.tsx
@@ -1,8 +1,102 @@
 import React from 'react'
 import styles from './page.module.css'
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+interface PageProps {
+    params: { game: string }
+}
+
+
+interface GameProps {
+    General: {
+        Title: string;
+        Link: string;
+        imgSrc: string;
+        GamePrice: string | null;
+        DiscountOriginalPrice: string | null;
+        FinalPrice: string | null;
+        gameDiscount: boolean | null;
+        Keywords: gamesKeywords[]
+
+    }
+
+    About: {
+        Description: string;
+        Wikipedia: string;
+    }
+
+    Extra: {
+        Description: string;
+        Images: [
+            {
+                image: string | null;
+            }
+        ]
+        Videos: [
+            {
+                video: string | null;
+            }
+        ]
+        DLCS: [
+            {
+                name: string;
+                discount: boolean;
+                discountPrice: string[];
+                originalDiscountPrices: string[];
+                price: string;
+            }
+        ]
+    }
+
+    Requirements: {
+        Maximum: [
+            {
+                Req: string;
+            }
+        ]
+
+        Minimum: [
+            {
+                Req: string;
+            }
+        ]
+        Requirements: [
+            {
+                Req: string;
+            }
+        ]
+    }
+}
+
+interface gamesKeywords {
+    keyword: string;
+}
+
+
+async function getGame(gameTitle : string) {
+    const response = await fetch('http://localhost:3560/getGame', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({ game: gameTitle }), 
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return response.json(); 
+}
+
+export default async function page({ params }: PageProps) {
+
+    const game : GameProps = await getGame(params.game)
+
+
+    return (
+        <div className={styles.game}>
+            {game.Extra.DLCS[0]?.name}
+           
+        </div>
+    )
 }
