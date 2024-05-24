3a31caf0931cf8055cec12bc83d23b8928e9bc78
TosuSW <denisappleid12@gmail.com>
2024-05-24 20:01:48 +0300

    Using link instead of router because of ssr

--- zerogames/app/Small-Components/GameBox/GameBox.tsx
+++ zerogames/app/Small-Components/GameBox/GameBox.tsx
@@ -1,7 +1,6 @@
 import React, { MouseEventHandler } from 'react'
 import styles from './page.module.css'
-
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 import DiscountPriceBox from '../DiscountPriceBox/DiscountPriceBox';
 import Tag from '../Tag/Tag';
@@ -19,39 +18,42 @@ interface GameBoxProps {
 }
 
 export default function GameBox({ imgSrc, title, description, isDiscount,
-     discountOriginalPrice, finalPrice, gamePrice, keyword1, keyword2 }: GameBoxProps) {
+    discountOriginalPrice, finalPrice, gamePrice, keyword1, keyword2 }: GameBoxProps) {
 
-        const router = useRouter()
 
     return (
-        <div className={styles.box} onClick={() => {router.push('/game/' + title)}}>
-            <img alt='Game Image' src={imgSrc}></img>
-            <h2 className={styles.title}>{title}</h2>
-            <p className={styles.description}>{description}</p>
+        <>
+            <Link style={{textDecoration: 'none'}} href={'/game/' + title}> <div className={styles.box} onClick={() => {  }}>
+                <img alt='Game Image' src={imgSrc}></img>
+                <h2 className={styles.title}>{title}</h2>
+                <p className={styles.description}>{description}</p>
 
-            <div className={styles.tags}>
-                <Tag tag={keyword1} />
-                <Tag tag={keyword2} />
-            </div>
-
-            <div className={styles.bottomcontent2}>
-
-                <div className={styles.price}>
-                    {isDiscount &&
-                        <DiscountPriceBox
-                            height='20px'
-                            originalPrice={parseFloat(discountOriginalPrice ?? "0")}
-                            discountPrice={parseFloat(finalPrice ?? "0")}
-                            discountPriceFS='0.8rem'
-                            originalPriceFS='0.8rem'
-                            percentageFontSize='1rem'
-                        />
-                    }
-                    {!isDiscount && <p className={styles.theprice}>{gamePrice}</p>}
-                    {!isDiscount && !gamePrice && <p className={styles.theprice}>15,00$</p>}
+                <div className={styles.tags}>
+                    <Tag tag={keyword1} />
+                    <Tag tag={keyword2} />
                 </div>
 
+                <div className={styles.bottomcontent2}>
+
+                    <div className={styles.price}>
+                        {isDiscount &&
+                            <DiscountPriceBox
+                                height='20px'
+                                originalPrice={parseFloat(discountOriginalPrice ?? "0")}
+                                discountPrice={parseFloat(finalPrice ?? "0")}
+                                discountPriceFS='0.8rem'
+                                originalPriceFS='0.8rem'
+                                percentageFontSize='1rem'
+                            />
+                        }
+                        {!isDiscount && <p className={styles.theprice}>{gamePrice}</p>}
+                        {!isDiscount && !gamePrice && <p className={styles.theprice}>15,00$</p>}
+                    </div>
+
+                </div>
             </div>
-        </div>
+            </Link>
+        </>
     )
+
 }
