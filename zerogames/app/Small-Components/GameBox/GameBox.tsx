e613bf6a8a4624dce28738141616929fdc8acf52
TosuSW <denisappleid12@gmail.com>
2024-05-25 16:35:28 +0300

    Fixing hydration
    - You cannot nest Link with div

--- zerogames/app/Small-Components/GameBox/GameBox.tsx
+++ zerogames/app/Small-Components/GameBox/GameBox.tsx
@@ -23,36 +23,38 @@ export default function GameBox({ imgSrc, title, description, isDiscount,
 
     return (
         <>
-            <Link style={{textDecoration: 'none'}} href={'/game/' + title}> <div className={styles.box}>
-                <img alt='Game Image' src={imgSrc}></img>
-                <h2 className={styles.title}>{title}</h2>
-                <p className={styles.description}>{description}</p>
+            <div className={styles.box}>
+              
+            <Link style={{ textDecoration: 'none' }} href={'/game/' + title}><img alt='Game Image' src={imgSrc}></img></Link>
+            <Link style={{ textDecoration: 'none' }} href={'/game/' + title}><h2 className={styles.title}>{title}</h2></Link>
+            <Link style={{ textDecoration: 'none' }} href={'/game/' + title}><p className={styles.description}>{description}</p></Link>
 
-                <div className={styles.tags}>
-                    <Tag tag={keyword1} />
-                    <Tag tag={keyword2} />
-                </div>
-
-                <div className={styles.bottomcontent2}>
-
-                    <div className={styles.price}>
-                        {isDiscount &&
-                            <DiscountPriceBox
-                                height='20px'
-                                originalPrice={parseFloat(discountOriginalPrice ?? "0")}
-                                discountPrice={parseFloat(finalPrice ?? "0")}
-                                discountPriceFS='0.8rem'
-                                originalPriceFS='0.8rem'
-                                percentageFontSize='1rem'
-                            />
-                        }
-                        {!isDiscount && <p className={styles.theprice}>{gamePrice}</p>}
-                        {!isDiscount && !gamePrice && <p className={styles.theprice}>15,00$</p>}
+                    <div className={styles.tags}>
+                        <Tag tag={keyword1} />
+                        <Tag tag={keyword2} />
                     </div>
 
-                </div>
+                    <div className={styles.bottomcontent2}>
+
+                        <div className={styles.price}>
+                            {isDiscount &&
+                                <DiscountPriceBox
+                                    height='20px'
+                                    originalPrice={parseFloat(discountOriginalPrice ?? "0")}
+                                    discountPrice={parseFloat(finalPrice ?? "0")}
+                                    discountPriceFS='0.8rem'
+                                    originalPriceFS='0.8rem'
+                                    percentageFontSize='1rem'
+                                />
+                            }
+                            {!isDiscount && <p className={styles.theprice}>{gamePrice}</p>}
+                            {!isDiscount && !gamePrice && <p className={styles.theprice}>15,00$</p>}
+                        </div>
+
+                    </div>
+
             </div>
-            </Link>
+
         </>
     )
 
