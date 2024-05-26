0d5b2c1c80ac3394979b48ff23838c6bdc9397f1
TosuSW <denisappleid12@gmail.com>
2024-05-26 23:49:56 +0300

    Category page dynamic metadata

--- zerogames/app/categories/[categorie]/[page]/page.tsx
+++ zerogames/app/categories/[categorie]/[page]/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 //components
 import PageCategoryGames from './PageCategoryGames/PageCategoryGames'
 import GamePage from '@/app/Global-Components/GamePage/GamePage'
+import { Metadata, ResolvingMetadata } from 'next'
 
 interface categoryPageProps {
   params: {
@@ -10,6 +11,22 @@ interface categoryPageProps {
     page: number
   }
 }
+
+export async function generateMetadata(
+  { params }: categoryPageProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+
+  return {
+    title: decodeURIComponent(params.categorie) + ' Page ' + params.page,
+    description: decodeURIComponent(params.categorie) + ' Games',
+    openGraph: {
+      images: ['https://i.ibb.co/7tw599R/Untitled-design-3.png'],
+
+    }
+  }
+}
+
 
 export default function page({ params }: categoryPageProps) {
   return (
--- zerogames/app/categories/[categorie]/page.tsx
+++ zerogames/app/categories/[categorie]/page.tsx
@@ -4,10 +4,27 @@ import styles from './page.module.css'
 //components
 import CategoryGames from './CategoryGames/CategoryGames'
 import GamePage from '@/app/Global-Components/GamePage/GamePage'
+import { Metadata, ResolvingMetadata } from 'next'
 
 interface CategoryProps {
     params: { categorie: string }
 }
+
+export async function generateMetadata(
+    { params }: CategoryProps,
+    parent: ResolvingMetadata
+  ): Promise<Metadata> {
+  
+    return {
+      title: decodeURIComponent(params.categorie),
+      description: decodeURIComponent(params.categorie) + ' Games',
+      openGraph: {
+        images: ['https://i.ibb.co/7tw599R/Untitled-design-3.png'],
+  
+      }
+    }
+  }
+  
 
 export default function page({ params }: CategoryProps) {
     return (
