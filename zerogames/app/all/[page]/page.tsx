76131a4b52982a0e5ed51ef72ee40db67062be2a
TosuSW <denisappleid12@gmail.com>
2024-05-26 23:51:47 +0300

    All games page dynamic metadata

--- zerogames/app/all/[page]/page.tsx
+++ zerogames/app/all/[page]/page.tsx
@@ -4,10 +4,31 @@ import React from 'react'
 //components
 import GamePage from '@/app/Global-Components/GamePage/GamePage'
 import AllGames from './AllGames/AllGames'
+import { Metadata, ResolvingMetadata } from 'next'
 
 interface PageProps{
   params: {page : number}
 }
+
+
+
+export async function generateMetadata(
+  { params }: PageProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+
+  return {
+    title: 'All Games' + ' Page ' + params.page,
+    description: 'All Games',
+    openGraph: {
+      images: ['https://i.ibb.co/7tw599R/Untitled-design-3.png'],
+
+    }
+  }
+}
+
+
+
 
 export default function page({params} : PageProps) {
   return (
