e59ff9541e24c29377fa651350d3c999f6637496
TosuSW <denisappleid12@gmail.com>
2024-05-26 23:43:44 +0300

    Update page.tsx

--- zerogames/app/page.tsx
+++ zerogames/app/page.tsx
@@ -7,7 +7,7 @@ import Showcase from "./Main-Page-Components/Showcase/Showcase";
 import MainShow from "./Main-Page-Components/Main/Main";
 
 export const metadata: Metadata = {
-  title: 'ZeroGames API',
+  title: 'ZeroGames Store',
   description: 'ZeroGames offers a free API that provides real-time data from Steam, enabling developers to access the latest game information and statistics effortlessly.',
   openGraph: {
     images:['https://i.ibb.co/9H8W9Xm/Screenshot-2024-05-26-232327.png']
