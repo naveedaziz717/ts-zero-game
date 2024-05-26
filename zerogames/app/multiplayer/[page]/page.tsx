23c02b4b3bd0ff64e57d3143302d98944b8f2f46
TosuSW <denisappleid12@gmail.com>
2024-05-26 23:46:30 +0300

    Multiplayer page dynamic metadata

--- zerogames/app/multiplayer/[page]/page.tsx
+++ zerogames/app/multiplayer/[page]/page.tsx
@@ -4,16 +4,24 @@ import styles from './page.module.css'
 //components
 import GamePage from '@/app/Global-Components/GamePage/GamePage'
 import PageMultiplayerGames from './PageMultiplayerGames/PageMultiplayerGames'
+import { Metadata } from 'next'
 
-interface PageProps{
-    params: {page: number}
+interface PageProps {
+  params: { page: number }
 }
 
+export const metadata: Metadata = {
+  title: 'ZeroGames Store',
+  description: 'Multiplayer Games',
+  openGraph: {
+    images: ['https://i.ibb.co/7tw599R/Untitled-design-3.png']
+  }
+}
 
-export default function page({params} : PageProps) {
+export default function page({ params }: PageProps) {
   return (
     <GamePage singles={true} theSingles={'Multiplayer/'.toLocaleLowerCase() + params.page} >
-    <PageMultiplayerGames params={params}/>
-   </GamePage>
+      <PageMultiplayerGames params={params} />
+    </GamePage>
   )
 }
--- zerogames/app/multiplayer/page.tsx
+++ zerogames/app/multiplayer/page.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 //components
 import GamePage from '../Global-Components/GamePage/GamePage'
 import MultiplayerGames from './MultiplayerGames/MultiplayerGames'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+    title: 'ZeroGames Store',
+    description: 'Multiplayer Games',
+    openGraph: {
+      images:['https://i.ibb.co/7tw599R/Untitled-design-3.png']
+    }
+  }
 
 export default function page() {
     return (
