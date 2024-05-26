7e1ce62cf8386e267534ad2d3ca4ec4b36b12430
TosuSW <denisappleid12@gmail.com>
2024-05-26 23:57:30 +0300

    Search page dynamic metadata

--- zerogames/app/search/[search]/[page]/page.tsx
+++ zerogames/app/search/[search]/[page]/page.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 //components
 import PageSearchGames from './PageSearchGames.tsx/PageSearchGames';
 import GamePage from '@/app/Global-Components/GamePage/GamePage';
+import { Metadata, ResolvingMetadata } from 'next';
 
 interface PageProps {
     params: {
@@ -11,6 +12,21 @@ interface PageProps {
         page: number;
     }
 }
+
+export async function generateMetadata(
+    { params }: PageProps,
+    parent: ResolvingMetadata
+  ): Promise<Metadata> {
+  
+    return {
+      title: 'Search: '+ params.search + ' Page ' + params.page,
+      description: 'You searched for: ' + params.search + ' Page ' + params.page,
+      openGraph: {
+        images: ['https://i.ibb.co/7tw599R/Untitled-design-3.png'],
+  
+      }
+    }
+  }
 
 export default function page({ params }: PageProps) {
     return (
--- zerogames/app/search/[search]/page.tsx
+++ zerogames/app/search/[search]/page.tsx
@@ -4,10 +4,27 @@ import styles from './page.module.css'
 //components
 import SearchGames from './SearchGames/SearchGames'
 import GamePage from '@/app/Global-Components/GamePage/GamePage'
+import { Metadata, ResolvingMetadata } from 'next'
 
 interface PageProps {
   params: { search: string }
 }
+
+export async function generateMetadata(
+  { params }: PageProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+
+  return {
+    title: 'Search: '+ params.search,
+    description: 'You searched for: ' + params.search,
+    openGraph: {
+      images: ['https://i.ibb.co/7tw599R/Untitled-design-3.png'],
+
+    }
+  }
+}
+
 
 export default function page({ params }: PageProps) {
   return (
